99946d892508d4c998710eaed336a86ebc557d6d
How Yu Chern <yhow0004@student.monash.edu>
2022-05-07 13:22:44 +0800

    Assignment 3 - Version 2 Date: 7/5/2022
    Notes:
    1) REQ1 unable to run issue fixed. This was due to the imports from ground class using a '*' wildcard character, which causes it to incorrectly import dirt class. The pull issue from git furthur worsened this issue, but has now fortunately been resolved. Will post this git issue to ed forum later today.
    
    2) REQ1 random tree initializer in Application class, treePopulate(), modified and repaired to fail fast (throw an exception) if it fails to correctly initialize the map randomly with trees (Sprouts).

--- src/game/Application.java
+++ src/game/Application.java
@@ -18,7 +18,12 @@ import game.actors.Goomba;
 import game.actors.Koopa;
 import game.actors.Player;
 import game.actors.Toad;
-import game.grounds.*;
+import game.grounds.Dirt;
+import game.grounds.Floor;
+import game.grounds.Sprout;
+import game.grounds.Sapling;
+import game.grounds.Mature;
+import game.grounds.Wall;
 import game.items.Coin;
 import game.items.PowerStar;
 import game.items.SuperMushroom;
@@ -37,33 +42,41 @@ public class Application {
 			FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Sprout(), new Sapling(), new Mature());
 
 			List<String> map = Arrays.asList(
-				"..........................................##..........+.........................",
-				"............+............+..................#...................................",
+				"..........................................##....................................",
 				"............................................#...................................",
-				".............................................##......................+..........",
+				"............................................#...................................",
+				".............................................##.................................",
 				"...............................................#................................",
 				"................................................#...............................",
-				".................+................................#.............................",
+				"..................................................#.............................",
 				".................................................##.............................",
 				"................................................##..............................",
-				".........+..............................+#____####.................+............",
+				"........................................+#____####..............................",
 				".......................................+#_____###++.............................",
 				".......................................+#______###..............................",
 				"........................................+#_____###..............................",
-				"........................+........................##.............+...............",
+				".................................................##.............................",
 				"...................................................#............................",
 				"....................................................#...........................",
-				"...................+.................................#..........................",
+				".....................................................#..........................",
 				"......................................................#.........................",
 				".......................................................##.......................");
 
 			GameMap gameMap = new GameMap(groundFactory, map);
 			world.addGameMap(gameMap);
 
-			//Populate GameMap with trees.
-			//Minimum 5 trees
-			//Maximum 10 trees
-//			treePopulate(gameMap, 5, 10);
+			/*
+			Populate GameMap with trees.
+			Minimum 5 trees
+			Maximum 10 trees
+			*/
+			try {
+				treePopulate(gameMap, 5, 10);
+			} catch (RuntimeException e) {
+				System.out.println(e.getMessage());
+			}
+
+			//Adding Player to the world.
 			Actor mario = new Player("Mario", 'm', 600);
 			mario.addItemToInventory(new Wrench());
 			world.addPlayer(mario, gameMap.at(33, 11));
@@ -80,42 +93,53 @@ public class Application {
 
 	}
 
-//	/**
-//	 * Method to randomly populate the game map with trees (Sprouts), with several constraints
-//	 * on tree count that apply to ensure that the map is not too crowded.
-//	 * @param gameMap The GameMap to populate with trees
-//	 * @param treeMin The minimum count of trees to put on the GameMap
-//	 * @param treeMax The maximum count of trees to put on the GameMap
-//	 */
-//	public static void treePopulate(GameMap gameMap, int treeMin, int treeMax){
-//		Random random = new Random();
-//		int randX = 0;
-//		int randY = 0;
-//		int currentTreeCount = 0;
-//		int totalTreeCount = 0;
-//
-//		//Assertions
-//		//Check if treeMin and treeMax are suitable - Otherwise throw an assertion
-//		assert treeMin < 0 : "The minimum count of trees allowed on the map must be more than 0.";
-//		//The maximum count of trees cannot exceed the map's area (width*height)
-//		assert treeMax > (gameMap.getXRange().max() * gameMap.getYRange().max()) : "The maximum count of trees allowed cannot exceed the map's total area.";
-//		assert treeMin > treeMax : "The minimum count of trees must be less than the maximum count of trees.";
-//
-//		//Pick a random amount of trees to spawn within a constraint.
-//		//This is done because random.nextInt(treeMin, treeMax) is not working.
-//		totalTreeCount = treeMin + random.nextInt(treeMax + 1 - treeMin);
-//
-//		//Pick random locations on the map to spawn Sprouts
-//		while (currentTreeCount < totalTreeCount){
-//			//Pick random location on map.
-//			randX = random.nextInt(gameMap.getXRange().max());
-//			randY = random.nextInt(gameMap.getYRange().max());
-//
-//			//Check if the location is fertile ground which Sprouts can grow on.
-//			if(gameMap.at(randX,randY).getGround().hasCapability(Status.FERTILE)) {
-//				gameMap.add('+', new NumberRange(randX, 1), new NumberRange(randY, 1));
-//				currentTreeCount++;
-//			}
-//		}
-//	}
+	/**
+	 * Method to randomly populate the game map with trees (Sprouts), with several constraints
+	 * on tree count that apply to ensure that the map is not too crowded.
+	 * @param gameMap The GameMap to populate with trees
+	 * @param treeMin The minimum count of trees to put on the GameMap
+	 * @param treeMax The maximum count of trees to put on the GameMap
+	 * @throws RuntimeException Throws exception when the code fails to initialize the correct random amount of trees.
+	 */
+	public static void treePopulate(GameMap gameMap, int treeMin, int treeMax) throws RuntimeException{
+		Random random = new Random();
+		int randX = 0;
+		int randY = 0;
+		int currentTreeCount = 0;
+		int totalTreeCount = 0;
+
+		//Assertions
+		//Check if treeMin is 1 or more.
+		assert treeMin < 0 : "The minimum count of trees allowed on the map must be more than 0.";
+		//Check if treeMax exceeds the map's area (width*height)
+		assert treeMax > (gameMap.getXRange().max() * gameMap.getYRange().max()) : "The maximum count of trees allowed cannot exceed the map's total area.";
+		//Check if treeMax is less than treeMin.
+		assert treeMin > treeMax : "The minimum count of trees must be less than the maximum count of trees.";
+
+		//Pick a random amount of trees to spawn within a constraint.
+		//This line of code below is done because random.nextInt(treeMin, treeMax) is not working.
+		totalTreeCount = treeMin + random.nextInt(treeMax + 1 - treeMin);
+
+		//Pick random locations on the map to spawn the random amount of trees (Sprouts).
+		/*This loop counter will cause the loop to terminate if it runs to many times whilst failing
+		to plant any trees. (fail fast)*/
+		int loopCounter = 0;
+		while (currentTreeCount < totalTreeCount && loopCounter < totalTreeCount + 50){
+			//Pick random location on map.
+			randX = random.nextInt(gameMap.getXRange().max());
+			randY = random.nextInt(gameMap.getYRange().max());
+
+			//Check if the location is fertile ground which Sprouts can grow on.
+			if(gameMap.at(randX,randY).getGround().hasCapability(Status.FERTILE)) {
+				gameMap.add('+', new NumberRange(randX, 1), new NumberRange(randY, 1));
+				currentTreeCount++;
+			}
+			loopCounter++;
+		}
+
+		//Throw exception if the loop fails to initialize the correct amount of trees.
+		if (currentTreeCount != totalTreeCount) {
+			throw new RuntimeException("Incorrect number of trees initialized on map. Expected: " + totalTreeCount + ", Trees Initialized: " + currentTreeCount + ".");
+		}
+	}
 }
