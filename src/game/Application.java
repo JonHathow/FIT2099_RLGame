98b185b35ea05bb0aee569bde341a2f9b149aa12
How Yu Chern <yhow0004@student.monash.edu>
2022-05-22 12:55:47 +0800

    Assignment 3 - Version 14 Date: 22/5/2022
    Notes:
    1)Added foundation for Random Populator class and interface.

--- src/game/Application.java
+++ src/game/Application.java
@@ -76,17 +76,6 @@ public class Application {
 			//Let Warp Pipe Manager know about the Lava Zone map, for identifying the Lava Zone Warp Pipe.
 			WarpPipeManager.getInstance().setLavaMap(lavaZone);
 
-			/*
-			Populate GameMap with trees.
-			Minimum 5 trees
-			Maximum 10 trees
-			*/
-			try {
-				treePopulate(gameMap, 5, 10);
-			} catch (RuntimeException e) {
-				System.out.println(e.getMessage());
-			}
-
 			//Initializing Peach and Bowser
 			lavaZone.at(45, 6).addActor(new Bowser());
 			lavaZone.at(48, 6).addActor(new PrincessPeach());
@@ -123,55 +112,5 @@ public class Application {
 
 
 			world.run();
-	}
-
-	/**
-	 * Method to randomly populate the game map with trees (Sprouts), with several constraints
-	 * on tree count that apply to ensure that the map is not too crowded.
-	 * @param gameMap The GameMap to populate with trees
-	 * @param treeMin The minimum count of trees to put on the GameMap
-	 * @param treeMax The maximum count of trees to put on the GameMap
-	 * @throws RuntimeException Throws exception when the code fails to initialize the correct random amount of trees.
-	 */
-	public static void treePopulate(GameMap gameMap, int treeMin, int treeMax) throws RuntimeException{
-		Random random = new Random();
-		int randX = 0;
-		int randY = 0;
-		int currentTreeCount = 0;
-		int totalTreeCount = 0;
-
-		//Assertions
-		//Check if treeMin is 1 or more.
-		assert treeMin < 0 : "The minimum count of trees allowed on the map must be more than 0.";
-		//Check if treeMax exceeds the map's area (width*height)
-		assert treeMax > (gameMap.getXRange().max() * gameMap.getYRange().max()) : "The maximum count of trees allowed cannot exceed the map's total area.";
-		//Check if treeMax is less than treeMin.
-		assert treeMin > treeMax : "The minimum count of trees must be less than the maximum count of trees.";
-
-		//Pick a random amount of trees to spawn within a constraint.
-		//This line of code below is done because random.nextInt(treeMin, treeMax) is not working.
-		totalTreeCount = treeMin + random.nextInt(treeMax + 1 - treeMin);
-
-		//Pick random locations on the map to spawn the random amount of trees (Sprouts).
-		/*This loop counter will cause the loop to terminate if it runs to many times whilst failing
-		to plant any trees. (fail fast)*/
-		int loopCounter = 0;
-		while (currentTreeCount < totalTreeCount && loopCounter < totalTreeCount + 50){
-			//Pick random location on map.
-			randX = random.nextInt(gameMap.getXRange().max());
-			randY = random.nextInt(gameMap.getYRange().max());
-
-			//Check if the location is fertile ground which Sprouts can grow on.
-			if(gameMap.at(randX,randY).getGround().hasCapability(Status.FERTILE)) {
-				gameMap.add('+', new NumberRange(randX, 1), new NumberRange(randY, 1));
-				currentTreeCount++;
-			}
-			loopCounter++;
-		}
-
-		//Throw exception if the loop fails to initialize the correct amount of trees.
-		if (currentTreeCount != totalTreeCount) {
-			throw new RuntimeException("Incorrect number of trees initialized on map. Expected: " + totalTreeCount + ", Trees Initialized: " + currentTreeCount + ".");
-		}
 	}
 }
---
+++ src/game/populate/PopulateCapable.java
@@ -0,0 +1,5 @@
+package game.populate;
+
+public interface PopulateCapable {
+    
+}
